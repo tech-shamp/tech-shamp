d0873c85134869e06d87e8162e3e248cefb1a9e5
iBOY <iboy@iboy.iboy>
2022-05-16 12:30:06 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/* =======================================
+    Creating A Portfolio Tabbed Component
+   ======================================= */
+
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
+
+p_btns.addEventListener("click", (e) => {
+  const p_btn_click = e.target;
+
+  p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
+
+  p_btn_click.classList.add("p-btn-active");
+  console.log(p_btn_click);
+
+  const btn_num = p_btn_click.dataset.btnNum;
+  console.log(btn_num);
+
+  const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+
+  p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
+
+  img_active.forEach((curElem) =>
+    curElem.classList.remove("p-image-not-active"),
+  );
+});
