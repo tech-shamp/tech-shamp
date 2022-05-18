23d596b9d993fae5a335f21c25c468cd909ab418
tech-shamp <shampytechnical@gmail.com>
2022-05-18 15:44:11 +0500

    minor changes

--- js/app.js
+++ js/app.js
@@ -28,7 +28,14 @@ p_btns.addEventListener("click", (e) => {
 
 // SWIPER
 var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
 });
